eb14dacc42a1b1a31f4417386444a4c5a4f9f94c
arshasherin <arshasherin171@gmail.com>
2024-11-28 20:20:49 +0530

    update add_product.dart

--- lib/views/admin/product/add_product.dart
+++ lib/views/admin/product/add_product.dart
@@ -84,31 +84,43 @@ class AddProductDrawer extends StatelessWidget {
                   children: [
                     Expanded(
                       child: DropdownButtonFormField<CategoryModel>(
-                          decoration: const InputDecoration(labelText: 'Category', border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10)))),
-                          value: vm.selectedCategory, // Expecting CategoryModel
-                          items: vm.categories
-                              .map((category) => DropdownMenuItem<CategoryModel>(
-                                    value: category, // Pass the CategoryModel object
-                                    child: Text(category.name.toString()), // Display the category name
-                                  ))
-                              .toList(),
-                          onChanged: (value) {
-                            if (value != null) {
-                              vm.selectCategory(value); // Pass the selected CategoryModel
-                            }
-                          }),
+                        decoration: const InputDecoration(
+                          labelText: 'Category',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                        ),
+                        value: vm.categories.contains(vm.selectedCategory) ? vm.selectedCategory : null,
+                        items: vm.categories.map((category) {
+                          return DropdownMenuItem<CategoryModel>(
+                            value: category,
+                            child: Text(category.name.toString()),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          if (value != null) {
+                            vm.selectCategory(value); // Ensure this updates vm.selectedCategory
+                          }
+                        },
+                      ),
                     ),
                     if (vm.selectedCategory?.name != null) 10.width,
-                    if (vm.selectedCategory?.name != null) IconButton(onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return  Category(isUpdate: true,category: vm.selectedCategory,);
+                    if (vm.selectedCategory?.name != null)
+                      IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Category(
+                                  isUpdate: true,
+                                  category: vm.selectedCategory,
+                                );
+                              },
+                            );
                           },
-                        ); 
-                    }, icon:const Icon(Icons.update)),
+                          icon: const Icon(Icons.update)),
                     if (vm.selectedCategory?.name != null) 5.width,
-                    if (vm.selectedCategory?.name != null) IconButton(onPressed: () {}, icon:const Icon(Icons.delete)),
+                    if (vm.selectedCategory?.name != null) IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
                     10.width,
                     CustomButton(
                       height: 40,
@@ -120,7 +132,9 @@ class AddProductDrawer extends StatelessWidget {
                         showDialog(
                           context: context,
                           builder: (BuildContext context) {
-                            return const Category(isUpdate: false,);
+                            return const Category(
+                              isUpdate: false,
+                            );
                           },
                         );
                       },
